9693fccf3e2b180076aa9ba42e96fbe6ea4e1668
Pyla, L. (Leo Nicolas, Student X-TCS) <62517091+Lpyla@users.noreply.github.com>
2024-11-22 17:36:23 +0100

    Create TH_v1.py

---
+++ TTN_TEAM/TH Server/TH_v1.py
@@ -0,0 +1,44 @@
+import json
+import datetime
+import paho.mqtt.client as mqtt
+
+
+
+
+APP_ID="sensor-downlink@ttn"
+ACCESS_KEY = "NNSXS.HTPOIGZA2Q6TSEOUESE5VABHALWKTHDMTYLW3TA.CATEX5UKOFMQG5HVZSG3P3VY3WORZVQ32R56BXJMY7TK4A5VIPXA"
+
+TOPIC = f"v3/{APP_ID}/devices/sensor-test/up"
+print(TOPIC)
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Connected with result code {reason_code}")
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(TOPIC)
+
+def on_message(client, userdata, msg):
+
+    print("ici")
+    print(msg)
+    
+    #p = json.loads(msg.payload)
+    #
+    temperatures.append(p["uplink_message"]["decoded_payload"]["temperature"])
+    #times.append(datetime.datetime.now())
+#
+    #fig, ax = plt.subplots()
+    #ax.plot(times, temperatures)
+#
+    #plt.show()
+    
+
+mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+mqttc.username_pw_set(APP_ID, ACCESS_KEY)
+
+mqttc.connect("eu1.cloud.thethings.network", 1883)
+
+mqttc.loop_forever()
