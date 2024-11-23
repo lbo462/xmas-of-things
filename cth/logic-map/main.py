3cdafc04b6b593b6cf6f81078f17e4da5c658fcd
gituser <gituser@mail.com>
2024-11-23 10:59:01 +0100

    Created ctx manager

--- cth/logic-map/main.py
+++ cth/logic-map/main.py
@@ -1,16 +1,17 @@
 import os
+import time
 import logging
 from dotenv import load_dotenv
-from ttn_access_layer import TTNAccessLayer, TTNMessage
+from ttn_access_layer import get_ttn_access_layer, TTNMessage
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 load_dotenv()
 
-TTN_APP_ID = os.getenv('TTN_APP_ID')
-TTN_API_KEY = os.getenv('TTN_API_KEY')
-TTN_BASE_URL = os.getenv('TTN_BASE_URL')
-TTN_SENSORS_TOPIC = os.getenv('TTN_SENSORS_TOPIC')
+TTN_APP_ID = os.getenv("TTN_APP_ID")
+TTN_API_KEY = os.getenv("TTN_API_KEY")
+TTN_BASE_URL = os.getenv("TTN_BASE_URL")
+TTN_SENSORS_TOPIC = os.getenv("TTN_SENSORS_TOPIC")
 
 
 def on_message(message: TTNMessage):
@@ -18,14 +19,10 @@ def on_message(message: TTNMessage):
 
 
 def main():
-    ttn = TTNAccessLayer(
-        TTN_APP_ID,
-        TTN_API_KEY,
-        TTN_BASE_URL,
-        TTN_SENSORS_TOPIC,
-        on_message=on_message
-    )
-    ttn.loop()
+    with get_ttn_access_layer(
+        TTN_APP_ID, TTN_API_KEY, TTN_BASE_URL, TTN_SENSORS_TOPIC, on_message=on_message
+    ):
+        time.sleep(10)
 
 
 if __name__ == "__main__":
--- cth/logic-map/ttn_access_layer.py
+++ cth/logic-map/ttn_access_layer.py
@@ -1,9 +1,10 @@
 import json
+import logging
 import paho.mqtt.client as mqtt
-from typing import Callable
+from typing import Callable, ContextManager
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
-import logging
+from contextlib import contextmanager
 
 
 logger = logging.getLogger(__name__)
@@ -20,15 +21,20 @@ class TTNMessage:
     temperature: int
 
 
-class TTNAccessLayer:
+class _TTNAccessLayer:
     """
     TTN access layer handling the connection to TTN together with some payload decoder functions.
-    Use instance.loop() to start listening for messages.
+    Do not call directly, use the context manager instead.
     """
 
-
     def __init__(
-        self, app_id: str, api_key: str, addr: str, topic, on_message: Callable[[TTNMessage], None], port: int = 1883
+        self,
+        app_id: str,
+        api_key: str,
+        addr: str,
+        topic,
+        on_message: Callable[[TTNMessage], None] | None = None,
+        port: int = 1883,
     ):
         """
         Creates an access layer to TTN
@@ -56,26 +62,69 @@ class TTNAccessLayer:
 
         return on_connect
 
-    def _get_on_message(self, on_message_callback: Callable[[TTNMessage], None]):
+    def _get_on_message(
+        self, on_message_callback: Callable[[TTNMessage], None] | None = None
+    ):
         """Create the on_message function for the mqtt client"""
 
         def on_message(client, userdata, msg):
-            logger.debug(f"Message received on {self} (payload length: {len(msg.payload)})")
+            logger.debug(
+                f"Message received on {self} (payload length: {len(msg.payload)})"
+            )
 
-            payload = json.loads(msg.payload)
-            decoded_payload = payload["uplink_message"]["decoded_payload"]
-            ttn_message = TTNMessage.from_dict(decoded_payload)
-            logger.debug(f"Payload decoded, triggering callback function with {ttn_message}")
+            if on_message_callback:
+                payload = json.loads(msg.payload)
+                decoded_payload = payload["uplink_message"]["decoded_payload"]
+                ttn_message = TTNMessage.from_dict(decoded_payload)
+                logger.debug(
+                    f"Payload decoded, triggering callback function with {ttn_message}"
+                )
+                on_message_callback(ttn_message)
 
-            on_message_callback(ttn_message)
+            else:
+                logger.debug(f"No callback function to call, continuing ...")
 
         return on_message
 
-    def loop(self):
-        """Start listening forever and ever!"""
+    def start(self):
+        """Start listening channel"""
+        self._client.loop_start()
         logger.info(f"{self} started listening")
-        self._client.loop_forever()
+
+    def stop(self):
+        """Stop listening"""
+        self._client.loop_stop()
+        logger.info(f"{self} stopped listening")
 
     def __str__(self) -> str:
         return f"Client to {self._topic}"
 
+
+@contextmanager
+def get_ttn_access_layer(
+    app_id: str,
+    api_key: str,
+    addr: str,
+    topic,
+    on_message: Callable[[TTNMessage], None],
+    port: int = 1883,
+) -> ContextManager[_TTNAccessLayer]:
+    """
+    Creates an access layer to TTN
+    :param app_id: Of the form "app_name@ttn"
+    :param api_key: API key for the given TTN app
+    :param addr: Base URL for accessing TTN
+    :param topic: Channel to which this access layer subscribes
+    :param on_message: Callback function to trigger when a message is received
+    """
+    ttn = _TTNAccessLayer(
+        app_id=app_id,
+        api_key=api_key,
+        addr=addr,
+        topic=topic,
+        on_message=on_message,
+        port=port,
+    )
+    ttn.start()
+    yield ttn
+    ttn.stop()
